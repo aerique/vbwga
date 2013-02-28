f09f98ba5ef5ac6809806a10dbf9ab90407f8491
aerique <aerique@xs4all.nl>
2013-02-28 23:33:46 +0100

    Add make-dist-vbwga.sh.

---
+++ make-dist-vbwga.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+tar cvfz vbwga-1.0.tar.gz victory-boogie-woogie-genetic-algorithm.lisp \
+                          victory-boogie-woogie-genetic-algorithm.html \
+                          start-drawing.bat start-drawing.sh           \
+                          quicklisp.lisp vbw-example.pdf               \
+                          reference-pictures/victory-boogie-woogie-marie-ll-flickr-512x512-rotated-45.png
